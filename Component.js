901d6ee6afc2d909d55c4b6fff96e382a53d1d8f
clmsemprebela <cledsondelimamafra@gmail.com>
2026-05-18 17:25:08 +0000

    Update Component.js

--- Component.js
+++ Component.js
@@ -1,23 +1,16 @@
 sap.ui.define([
-    "sap/ui/core/UIComponent",
-    "sap/ui/core/mvc/XMLView"
-], function (UIComponent, XMLView) {
+    "sap/ui/core/UIComponent"
+], function (UIComponent) {
     "use strict";
 
     return UIComponent.extend("ferias1000.Component", {
         metadata: {
-            interfaces: ["sap.ui.core.IAsyncContentCreation"],
             manifest: "json"
         },
 
         init: function () {
+            // Chama a função init do pai
             UIComponent.prototype.init.apply(this, arguments);
-        },
-
-        createContent: function () {
-            return XMLView.create({
-                viewName: "ferias1000.Main"
-            });
         }
     });
 });
