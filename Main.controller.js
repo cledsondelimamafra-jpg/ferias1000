c6db5eb26bf75e98e4f4f44bbb69018e996cb186
clmsemprebela <cledsondelimamafra@gmail.com>
2026-05-18 18:17:52 +0000

    Delete Main.controller.js

--- Main.controller.js
+++
@@ -1,9 +0,0 @@
-sap.ui.define([
-    "sap/ui/core/mvc/Controller",
-    "sap/ui/model/json/JSONModel",
-    "sap/m/MessageToast"
-], function (Controller, JSONModel, MessageToast) {
-    "use strict";
-
-    return Controller.extend("ferias1000.Main", {
-// ... resto do seu código igualzinho
