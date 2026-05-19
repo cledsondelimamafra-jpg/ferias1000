f7aa475ecdd2f1ffa5222092f99e361ebc8ffa07
clmsemprebela <cledsondelimamafra@gmail.com>
2026-05-19 14:15:53 +0000

    Update Component.js

--- Component.js
+++ Component.js
@@ -1,7 +1,6 @@
 sap.ui.define([
-    "sap/ui/core/UIComponent",
-    "sap/ui/model/json/JSONModel"
-], function (UIComponent, JSONModel) {
+    "sap/ui/core/UIComponent"
+], function (UIComponent) {
     "use strict";
 
     return UIComponent.extend("ferias1000.Component", {
@@ -11,27 +10,8 @@ sap.ui.define([
         },
 
         init: function () {
+            // Apenas inicializa o componente base
             UIComponent.prototype.init.apply(this, arguments);
-
-            // Tenta recuperar registros anteriores salvos no LocalStorage antes de gerar a estrutura limpa
-            var sDocumentosSalvos = localStorage.getItem("ferias1000_documentos");
-            var sReservasSalvas = localStorage.getItem("ferias1000_reservas");
-            var sPassagensSalvas = localStorage.getItem("ferias1000_passagens");
-
-            var oData = {
-                local: { cidade: "Aguardando..." },
-                clima: { temp: "--" },
-                lugares: [],
-                // Se houver dados no dispositivo converte a String JSON, se não, inicia lista limpa
-                documentos: sDocumentosSalvos ? JSON.parse(sDocumentosSalvos) : [],
-                reservas: sReservasSalvas ? JSON.parse(sReservasSalvas) : [],
-                passagens: sPassagensSalvas ? JSON.parse(sPassagensSalvas) : []
-            };
-
-            var oModel = new JSONModel(oData);
-            // Habilita TwoWay Binding agressivo para garantir atualização imediata em tempo de execução
-            oModel.setDefaultBindingMode(sap.ui.model.BindingMode.TwoWay);
-            this.setModel(oModel, "view");
         }
     });
 });
