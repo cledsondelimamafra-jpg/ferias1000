cba2fa5f92daaae2ce10ba8b98a69112d43d26b9
clmsemprebela <cledsondelimamafra@gmail.com>
2026-05-19 16:33:19 +0000

    Update Component.js

--- Component.js
+++ Component.js
@@ -2,9 +2,14 @@ sap.ui.define([
     "sap/ui/core/UIComponent"
 ], function (UIComponent) {
     "use strict";
+
     return UIComponent.extend("ferias1000.Component", {
-        metadata: { manifest: "json" },
+        metadata: {
+            manifest: "json"
+        },
+
         init: function () {
+            // Chama a função init da classe pai
             UIComponent.prototype.init.apply(this, arguments);
         }
     });
