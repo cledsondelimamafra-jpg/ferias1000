c69c85e53a9570e6ce5942eb115f18bf0b840677
clmsemprebela <cledsondelimamafra@gmail.com>
2026-05-19 14:45:41 +0000

    Update Main.controller.js

--- controller/Main.controller.js
+++ controller/Main.controller.js
@@ -8,7 +8,7 @@ sap.ui.define([
     return Controller.extend("ferias1000.controller.Main", {
 
         onInit: function () {
-            // Inicialização do modelo com dados vazios ou recuperados do LocalStorage
+            // Inicialização com dados locais
             var oData = {
                 documentos: JSON.parse(localStorage.getItem("ferias1000_documentos") || "[]"),
                 reservas: JSON.parse(localStorage.getItem("ferias1000_reservas") || "[]"),
@@ -17,67 +17,54 @@ sap.ui.define([
                 clima: { temp: "--" },
                 lugares: []
             };
-
             var oModel = new JSONModel(oData);
             this.getView().setModel(oModel, "view");
         },
 
-        // Função genérica de salvamento para qualquer aba
-        _persistirDados: function (sKey, sPath, sMensagem) {
+        onAfterRendering: function () {
+            // Garante que o mapa inicie após a renderização
+            if (!this._oMap) {
+                this.initMap();
+            }
+        },
+
+        initMap: function () {
+            var oMapDom = this.getView().byId("map") ? this.getView().byId("map").getDomRef() : document.getElementById("map");
+            if (oMapDom && !this._oMap) {
+                this._oMap = L.map(oMapDom).setView([-14.2350, -51.9253], 4);
+                L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+                    attribution: '&copy; OpenStreetMap'
+                }).addTo(this._oMap);
+            }
+        },
+
+        // Função de salvamento unificada
+        _salvarDados: function (sKey, sPath, sMensagem) {
             var oModel = this.getView().getModel("view");
-            oModel.updateBindings(true); // Garante que campos de input sejam lidos
+            oModel.updateBindings(true);
             var aDados = oModel.getProperty(sPath) || [];
             localStorage.setItem(sKey, JSON.stringify(aDados));
             MessageToast.show(sMensagem);
         },
 
-        onSalvarDocumentos: function () {
-            this._persistirDados("ferias1000_documentos", "/documentos", "Documentos salvos!");
-        },
+        onSalvarDocumentos: function () { this._salvarDados("ferias1000_documentos", "/documentos", "Documentos salvos!"); },
+        onSalvarReservas: function () { this._salvarDados("ferias1000_reservas", "/reservas", "Reservas salvas!"); },
+        onSalvarPassagens: function () { this._salvarDados("ferias1000_passagens", "/passagens", "Passagens salvas!"); },
 
-        onSalvarReservas: function () {
-            this._persistirDados("ferias1000_reservas", "/reservas", "Reservas salvas!");
-        },
-
-        onSalvarPassagens: function () {
-            this._persistirDados("ferias1000_passagens", "/passagens", "Passagens salvas!");
-        },
-
-        // Funções para adicionar linhas nas tabelas
         onAdicionarDocumento: function () {
-            var oModel = this.getView().getModel("view");
-            var a = oModel.getProperty("/documentos");
-            a.push({ tipo: "", status: "", arquivoNome: "", arquivoBase64: "" });
-            oModel.setProperty("/documentos", a);
+            var a = this.getView().getModel("view").getProperty("/documentos");
+            a.push({ tipo: "", status: "", arquivoBase64: "" });
+            this.getView().getModel("view").setProperty("/documentos", a);
         },
-
         onAdicionarReserva: function () {
-            var oModel = this.getView().getModel("view");
-            var a = oModel.getProperty("/reservas");
-            a.push({ hotel: "", periodo: "", arquivoNome: "", arquivoBase64: "" });
-            oModel.setProperty("/reservas", a);
+            var a = this.getView().getModel("view").getProperty("/reservas");
+            a.push({ hotel: "", periodo: "", arquivoBase64: "" });
+            this.getView().getModel("view").setProperty("/reservas", a);
         },
-
         onAdicionarPassagem: function () {
-            var oModel = this.getView().getModel("view");
-            var a = oModel.getProperty("/passagens");
-            a.push({ voo: "", info: "", arquivoNome: "", arquivoBase64: "" });
-            oModel.setProperty("/passagens", a);
-        },
-
-        // Função de upload (mantida para você processar os arquivos)
-        onUploadArquivo: function (oEvent) {
-            var oFile = oEvent.getParameter("files")[0];
-            var oContext = oEvent.getSource().getBindingContext("view");
-            if (oFile) {
-                var oReader = new FileReader();
-                oReader.onload = function (e) {
-                    oContext.getModel().setProperty(oContext.getPath() + "/arquivoBase64", e.target.result);
-                    oContext.getModel().setProperty(oContext.getPath() + "/arquivoNome", oFile.name);
-                    MessageToast.show("Arquivo anexado!");
-                };
-                oReader.readAsDataURL(oFile);
-            }
+            var a = this.getView().getModel("view").getProperty("/passagens");
+            a.push({ voo: "", info: "", arquivoBase64: "" });
+            this.getView().getModel("view").setProperty("/passagens", a);
         }
     });
 });
