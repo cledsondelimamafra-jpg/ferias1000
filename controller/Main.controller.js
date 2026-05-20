27bb07ba531308fc616b65436f06c6570e993dd8
clmsemprebela <cledsondelimamafra@gmail.com>
2026-05-20 12:47:59 +0000

    Update Main.controller.js

--- controller/Main.controller.js
+++ controller/Main.controller.js
@@ -4,24 +4,37 @@ sap.ui.define([
     "sap/m/MessageToast"
 ], function (Controller, JSONModel, MessageToast) {
     "use strict";
+
     return Controller.extend("ferias1000.controller.Main", {
         onInit: function () {
+            // Recupera dados salvos ou cria lista vazia
+            var aDocs = JSON.parse(localStorage.getItem("docs_salvos") || "[]");
+            
             var oData = {
                 novoDoc: { tipo: "" },
-                documentos: JSON.parse(localStorage.getItem("docs_salvos") || "[]")
+                documentos: aDocs
             };
-            this.getView().setModel(new JSONModel(oData), "view");
+            
+            var oModel = new JSONModel(oData);
+            this.getView().setModel(oModel, "view");
         },
+
         onSalvarDocs: function () {
             var oModel = this.getView().getModel("view");
             var oNovo = oModel.getProperty("/novoDoc");
             var aDocs = oModel.getProperty("/documentos");
-            if (oNovo.tipo) {
+
+            if (oNovo.tipo && oNovo.tipo.trim() !== "") {
                 aDocs.push({ tipo: oNovo.tipo });
                 localStorage.setItem("docs_salvos", JSON.stringify(aDocs));
+                
+                // Atualiza o modelo na tela
                 oModel.setProperty("/documentos", aDocs);
                 oModel.setProperty("/novoDoc/tipo", "");
-                MessageToast.show("Salvo!");
+                
+                MessageToast.show("Documento salvo!");
+            } else {
+                MessageToast.show("Por favor, digite um nome.");
             }
         }
     });
